313941b2d740006f7dabfb9d4d8878714bae8621
James Bowles <jamesrbowles@gmail.com>
2023-01-04 17:37:18 +0100

    work in progress

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,78 +1,83 @@
-import { useRef } from "react";
+import { useRef, forwardRef } from "react";
 import { BsSuitSpadeFill } from "react-icons/bs";
 
 import "./TaskCard.css";
 
 import TaskBtns from "../TaskBtns/TaskBtns";
 
-const TaskCard = ({
-  task,
-  tasks,
-  displayTaskBtns,
-  showTaskBtns,
-  deleteTask,
-  completeTask,
-  enterEditMode,
-  spreadTasks,
-  taskSpread,
-}) => {
-  const firstKey = tasks[tasks.length - 1].id;
+const TaskCard = forwardRef(
+  (
+    {
+      task,
+      tasks,
+      displayTaskBtns,
+      showTaskBtns,
+      deleteTask,
+      completeTask,
+      enterEditMode,
+      spreadTasks,
+      taskSpread,
+    },
+    ref
+  ) => {
+    const firstKey = tasks[tasks.length - 1].id;
 
-  const translateX =
-    Math.floor(Math.random() * 12) * (Math.round(Math.random()) ? 1 : -1);
-  const translateY =
-    Math.floor(Math.random() * 4) * (Math.round(Math.random()) ? 1 : -1);
-  const rotate =
-    Math.floor(Math.random() * 6) * (Math.round(Math.random()) ? 1 : -1);
+    const translateX =
+      Math.floor(Math.random() * 12) * (Math.round(Math.random()) ? 1 : -1);
+    const translateY =
+      Math.floor(Math.random() * 4) * (Math.round(Math.random()) ? 1 : -1);
+    const rotate =
+      Math.floor(Math.random() * 6) * (Math.round(Math.random()) ? 1 : -1);
 
-  const stagger = useRef({
-    transform: `translateX(${translateX}%) translateY(${translateY}%) rotate(${rotate}deg)`,
-  });
+    const stagger = useRef({
+      transform: `translateX(${translateX}%) translateY(${translateY}%) rotate(${rotate}deg)`,
+    });
 
-  return (
-    <div className="each-card">
-      <div
-        className={
-          taskSpread && task.completed
-            ? "spread-task task-card task-card-size-spread completed"
-            : taskSpread
-            ? "spread-task task-card task-card-size-spread"
-            : task.completed
-            ? "task-card completed"
-            : "task-card"
-        }
-        style={stagger.current}
-        onClick={displayTaskBtns}
-      >
-        {/*         <BsSuitSpadeFill className="card-suit-top" /> */}
-        <div className="task-text">
-          <h1>{task.name}</h1>
-          <p>{task.description}</p>
-          {/*      <div className="center-line"></div> */}
+    return (
+      <div className="each-card" ref={ref}>
+        <div
+          className={
+            taskSpread && task.completed
+              ? "spread-task task-card task-card-size-spread completed"
+              : taskSpread
+              ? "spread-task task-card task-card-size-spread"
+              : task.completed
+              ? "task-card completed"
+              : "task-card"
+          }
+          style={stagger.current}
+          onClick={displayTaskBtns}
+        >
+          {/*         <BsSuitSpadeFill className="card-suit-top" /> */}
+          <div className="task-text">
+            <h1>{task.name}</h1>
+            <p>{task.description}</p>
+            {/*      <div className="center-line"></div> */}
+          </div>
+          {/*         <BsSuitSpadeFill className="card-suit-bottom" /> */}
         </div>
-        {/*         <BsSuitSpadeFill className="card-suit-bottom" /> */}
+        <div
+          className={
+            taskSpread
+              ? "task-card-container-copy task-card-size-spread"
+              : "task-card-container-copy"
+          }
+          style={stagger.current}
+        >
+          {firstKey === task.id && (
+            <TaskBtns
+              showTaskBtns={showTaskBtns}
+              deleteTask={deleteTask}
+              task={task}
+              completeTask={completeTask}
+              enterEditMode={enterEditMode}
+              spreadTasks={spreadTasks}
+            />
+          )}
+        </div>
       </div>
-      <div
-        className={
-          taskSpread
-            ? "task-card-container-copy task-card-size-spread"
-            : "task-card-container-copy"
-        }
-        style={stagger.current}
-      >
-        {firstKey === task.id && (
-          <TaskBtns
-            showTaskBtns={showTaskBtns}
-            deleteTask={deleteTask}
-            task={task}
-            completeTask={completeTask}
-            enterEditMode={enterEditMode}
-            spreadTasks={spreadTasks}
-          />
-        )}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default TaskCard;
--- src/components/TaskContainer/TaskContainer.js
+++ src/components/TaskContainer/TaskContainer.js
@@ -1,3 +1,5 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 import TaskCard from "../TaskCard/TaskCard";
 import "./TaskContainer.css";
 
@@ -20,32 +22,48 @@ const TaskContainer = ({
   displayForm,
 }) => {
   return (
-    <div
-      className={
-        taskSpread ? "task-container task-container-spread" : "task-container"
-      }
-    >
-      {tasks.map((task, index) => (
-        <TaskCard
-          task={task}
-          taskName={taskName}
-          taskDesc={taskDesc}
-          key={task.id}
-          displayTaskBtns={displayTaskBtns}
-          showTaskBtns={showTaskBtns}
-          tasks={tasks}
-          deleteTask={deleteTask}
-          completeTask={completeTask}
-          enterEditMode={enterEditMode}
-          spreadTasks={spreadTasks}
-          taskSpread={taskSpread}
+    <div>
+      <div
+        className={
+          taskSpread ? "task-container task-container-spread" : "task-container"
+        }
+      >
+        <DragDropContext>
+          <Droppable droppableId="characters">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {tasks.map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                    {(provided) => (
+                      <TaskCard
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                        task={task}
+                        taskName={taskName}
+                        taskDesc={taskDesc}
+                        displayTaskBtns={displayTaskBtns}
+                        showTaskBtns={showTaskBtns}
+                        tasks={tasks}
+                        deleteTask={deleteTask}
+                        completeTask={completeTask}
+                        enterEditMode={enterEditMode}
+                        spreadTasks={spreadTasks}
+                        taskSpread={taskSpread}
+                      />
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <IoAddCircleOutline
+          className={addTaskBtnPosition()}
+          onClick={displayForm}
         />
-      ))}
-      <IoAddCircleOutline
-        className={addTaskBtnPosition()}
-        onClick={displayForm}
-      />
-      <div className="added-plus2"></div>
+        <div className="added-plus2"></div>
+      </div>
     </div>
   );
 };
